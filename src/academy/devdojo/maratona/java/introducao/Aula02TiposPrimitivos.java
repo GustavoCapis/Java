3aaf0609163af27619d999c5f72d960f703ee104
Gustavo Nepomuceno Capistrano <gustavo.capy@gmail.com>
2026-08-11 20:38:41 -0300

    feat: adiciona os 8 tipos de dados primitivos

--- src/academy/devdojo/maratona/java/introducao/Aula02TiposPrimitivos.java
+++ src/academy/devdojo/maratona/java/introducao/Aula02TiposPrimitivos.java
@@ -4,6 +4,14 @@ public class Aula02TiposPrimitivos {
     static void main(String[] args) {
         // int, double, float, char, byte, short, long, boolean
         int age = 10;
-        System.out.println("A idade é:"+age);
+        long longNumber = 1000000;
+        double doubleSalary = 2000;
+        float floatSalary = 2500;
+        byte byteAge = 10;
+        short shortAge = 10;
+        boolean verdadeiro = true;
+        boolean falso = false;
+        char character = 'M';
+        System.out.println("A idade é:"+age+" anos");
     }
 }
