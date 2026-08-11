78407f706ba7af76c1d4e23f0b748e9b8cda9df2
Gustavo Nepomuceno Capistrano <gustavo.capy@gmail.com>
2026-08-11 20:47:54 -0300

    feat: adiciona casting

--- src/academy/devdojo/maratona/java/introducao/Aula02TiposPrimitivos.java
+++ src/academy/devdojo/maratona/java/introducao/Aula02TiposPrimitivos.java
@@ -3,10 +3,10 @@ package academy.devdojo.maratona.java.introducao;
 public class Aula02TiposPrimitivos {
     static void main(String[] args) {
         // int, double, float, char, byte, short, long, boolean
-        int age = 10;
-        long longNumber = 1000000;
-        double doubleSalary = 2000;
-        float floatSalary = 2500;
+        int age = (int) 10000000000L; //casting
+        long longNumber = 1000000000;
+        double doubleSalary = 2000.00;
+        float floatSalary = 2500.00F;
         byte byteAge = 10;
         short shortAge = 10;
         boolean verdadeiro = true;
