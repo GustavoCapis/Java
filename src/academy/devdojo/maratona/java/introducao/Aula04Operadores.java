597b7fc6c3316a87094c648405b8a42ff46e91c4
Gustavo Nepomuceno Capistrano <gustavo.capy@gmail.com>
2026-08-12 11:07:05 -0300

    feat: adiciona operadores relacionais

--- src/academy/devdojo/maratona/java/introducao/Aula04Operadores.java
+++ src/academy/devdojo/maratona/java/introducao/Aula04Operadores.java
@@ -2,11 +2,20 @@ package academy.devdojo.maratona.java.introducao;
 
 public class Aula04Operadores {
     static void main(String[] args) {
-        // +, -, /, *
+        //aritiméticos: +, -, /, *, %
         double num1 = 10;
         double num2 = 20;
         double result = num1 / num2;
         System.out.println(result);
+
+        int modulo = 20 % 2;
+        System.out.println(modulo);
+
+        //lógicos: <, >, <=, >=, ==, !=
+        boolean isTenGreaterThanTwenty = 10 > 20;
+
+        System.out.println(isTenGreaterThanTwenty);
+
 
     }
 }
