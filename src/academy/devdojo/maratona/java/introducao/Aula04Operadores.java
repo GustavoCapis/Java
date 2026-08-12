3a54e9df7ab9f4de0ff1efc1ce137d7087634ebb
Gustavo Nepomuceno Capistrano <gustavo.capy@gmail.com>
2026-08-12 10:47:34 -0300

    feat: adiciona operadores aritiméticos

---
+++ src/academy/devdojo/maratona/java/introducao/Aula04Operadores.java
@@ -0,0 +1,12 @@
+package academy.devdojo.maratona.java.introducao;
+
+public class Aula04Operadores {
+    static void main(String[] args) {
+        // +, -, /, *
+        double num1 = 10;
+        double num2 = 20;
+        double result = num1 / num2;
+        System.out.println(result);
+
+    }
+}
