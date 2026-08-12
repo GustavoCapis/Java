d1a53f337a1285f0c456b9ffc2a907aeb1451ac6
Gustavo Nepomuceno Capistrano <gustavo.capy@gmail.com>
2026-08-12 15:36:23 -0300

    feat: adiciona exercicio sobre condicionais

---
+++ src/academy/devdojo/maratona/java/introducao/Aula05Exercicio.java
@@ -0,0 +1,18 @@
+package academy.devdojo.maratona.java.introducao;
+
+public class Aula05Exercicio {
+    static void main(String[] args) {
+        double anualSalary = 22000;
+        double tax;
+
+        if (anualSalary <= 34712) {
+            tax = 9.70 / 100;
+        } else if (anualSalary >= 34713 && anualSalary <= 68507) {
+            tax = 37.35 / 100;
+        } else {
+            tax = 49.50 / 100;
+        }
+        double debt =  anualSalary * tax;
+        System.out.println(debt);
+    }
+}
