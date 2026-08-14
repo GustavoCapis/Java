56ce1c7eb2eb5ad39ea8c1820ba601beae3a0e08
Gustavo Nepomuceno Capistrano <gustavo.capy@gmail.com>
2026-08-13 21:27:53 -0300

    feat: adiciona classe Professor

--- src/academy/devdojo/maratona/java/POO/test/EstudanteTest02.java
+++ src/academy/devdojo/maratona/java/POO/test/EstudanteTest02.java
@@ -2,17 +2,9 @@ package academy.devdojo.maratona.java.POO.test;
 
 import academy.devdojo.maratona.java.POO.dominio.Estudante;
 
-public class EstudanteTest {
+public class EstudanteTest02 {
     static void main(String[] args) {
         Estudante estudante = new Estudante();
-        estudante.nome = "Gustavo";
-        estudante.idade = 25;
-        estudante.sexo = 'M';
         System.out.println(estudante.nome);
-        System.out.println(estudante.idade);
-        System.out.println(estudante.sexo);
-
     }
-
 }
-
---
+++ src/academy/devdojo/maratona/java/POO/test/ProfessorTest01.java
@@ -0,0 +1,14 @@
+package academy.devdojo.maratona.java.POO.test;
+
+import academy.devdojo.maratona.java.POO.dominio.Professor;
+
+public class ProfessorTest01 {
+    static void main(String[] args) {
+        Professor professor = new Professor();
+        professor.nome = "Yuri";
+        professor.idade = 49;
+        professor.sexo = 'M';
+
+        System.out.println("Professor "+professor.nome+" idade: "+ professor.idade+" sexo: "+professor.sexo);
+    }
+}
