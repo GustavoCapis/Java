62c6483c7849227de527be943c92f2b9e8b6eb87
Gustavo Nepomuceno Capistrano <gustavo.capy@gmail.com>
2026-08-14 11:03:43 -0300

    feat: adiciona método com parâmetro

---
+++ src/academy/devdojo/maratona/java/POO/metodos/test/CalculadoraTest02.java
@@ -0,0 +1,10 @@
+package academy.devdojo.maratona.java.POO.metodos.test;
+
+import academy.devdojo.maratona.java.POO.metodos.dominio.Calculadora;
+
+public class CalculadoraTest02 {
+    static void main(String[] args) {
+        Calculadora calculadora = new Calculadora();
+        calculadora.MulTwoNum(2,8);
+    }
+}
