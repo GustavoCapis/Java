0deaa52ba8d740782f680a36b24d3ef46a7336fe
Gustavo Nepomuceno Capistrano <gustavo.capy@gmail.com>
2026-08-18 14:29:43 -0300

    feat: adiciona sobrecarga de construtores

--- src/academy/devdojo/maratona/java/POO/construtores/dominio/Anime.java
+++ src/academy/devdojo/maratona/java/POO/construtores/dominio/Anime.java
@@ -5,13 +5,18 @@ public class Anime {
     private String tipo;
     private int episodios;
     private String genero;
-
+    private String estudio;
 
     public Anime(String nome, String tipo, int episodios, String genero) {
         this.nome = nome;
         this.tipo = tipo;
         this.episodios = episodios;
         this.genero = genero;
+    }
+
+    public Anime(String nome, String tipo, int episodios, String genero, String estudio) {
+        this(nome, tipo, episodios, genero);
+        this.estudio = estudio;
     }
 
     public Anime() {
@@ -22,5 +27,6 @@ public class Anime {
         System.out.println("Tipo: " + this.tipo);
         System.out.println("Episódios: " + this.episodios);
         System.out.println("Gênero: " + this.genero);
+        System.out.println("Estudio: " + this.estudio);
     }
 }
--- src/academy/devdojo/maratona/java/POO/construtores/test/AnimeTest01.java
+++ src/academy/devdojo/maratona/java/POO/construtores/test/AnimeTest01.java
@@ -4,7 +4,7 @@ import academy.devdojo.maratona.java.POO.construtores.dominio.Anime;
 
 public class AnimeTest01 {
     public static void main(String[] args) {
-        Anime anime = new Anime("Haikyuu", "TV", 12, "Ação");
+        Anime anime = new Anime("Haikyuu", "TV", 12, "Ação", "IG Productions");
         anime.print();
 
     }
