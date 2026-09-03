22d087573938737b7c9faaaba5e44a87d01cca48
Gustavo Nepomuceno Capistrano <gustavo.capy@gmail.com>
2026-09-02 22:29:53 -0300

    refactor: refatora método de calcular imposto para parâmetro polimórfico

--- src/academy/devdojo/maratona/java/POO/polimorfismo/service/CalculadoraImposto.java
+++ src/academy/devdojo/maratona/java/POO/polimorfismo/service/CalculadoraImposto.java
@@ -1,22 +1,13 @@
 package academy.devdojo.maratona.java.POO.polimorfismo.service;
 
-import academy.devdojo.maratona.java.POO.polimorfismo.dominio.Computador;
-import academy.devdojo.maratona.java.POO.polimorfismo.dominio.Tomate;
+import academy.devdojo.maratona.java.POO.polimorfismo.dominio.Produto;
 
 public class CalculadoraImposto {
-    public static void calcularImpostoComputador(Computador computador) {
-        System.out.println("Relatório do imposto do computador");
-        double imposto = computador.calcularImposto();
-        System.out.println("Computador " + computador.getNome());
-        System.out.println("Valor " + computador.getValor());
-        System.out.println("Imposto a ser pago " + imposto);
-    }
-
-    public static void calcularImpostoTomate(Tomate tomate) {
-        System.out.println("Relatório do imposto do computador");
-        double imposto = tomate.calcularImposto();
-        System.out.println("Tomate " + tomate.getNome());
-        System.out.println("Valor " + tomate.getValor());
-        System.out.println("Imposto a ser pago " + imposto);
+    public static void calcularImposto(Produto produto) {
+        System.out.println("Relatório de imposto");
+        double imposto = produto.calcularImposto();
+        System.out.println("Produto: " + produto.getNome());
+        System.out.println("Preço: " + produto.getValor());
+        System.out.println("Imposto a ser pago: " + imposto);
     }
 }
--- src/academy/devdojo/maratona/java/POO/polimorfismo/test/ProdutoTest01.java
+++ src/academy/devdojo/maratona/java/POO/polimorfismo/test/ProdutoTest01.java
@@ -1,16 +1,23 @@
 package academy.devdojo.maratona.java.POO.polimorfismo.test;
 
 import academy.devdojo.maratona.java.POO.polimorfismo.dominio.Computador;
+import academy.devdojo.maratona.java.POO.polimorfismo.dominio.Televisao;
 import academy.devdojo.maratona.java.POO.polimorfismo.dominio.Tomate;
 import academy.devdojo.maratona.java.POO.polimorfismo.service.CalculadoraImposto;
 
 public class ProdutoTest01 {
     static void main(String[] args) {
         Computador computador = new Computador("Dell G15", 4000);
+
         Tomate tomate = new Tomate("Tomate vermelho", 10);
-        CalculadoraImposto.calcularImpostoComputador(computador);
+
+        Televisao tv = new Televisao("TV Samsung 50\"", 6000);
+
+        CalculadoraImposto.calcularImposto(computador);
         System.out.println("--------------------");
-        CalculadoraImposto.calcularImpostoTomate(tomate);
+        CalculadoraImposto.calcularImposto(tomate);
+        System.out.println("--------------------");
+        CalculadoraImposto.calcularImposto(tv);
 
     }
 }
